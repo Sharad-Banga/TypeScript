374733ddc72b54c66e61a57518d42aa3d85dff94
Sharad Banga <sharadiit03@gmail.com>
2025-02-17 18:50:22 +0530

    update

--- assi_4.tsx
+++ assi_4.tsx
@@ -10,3 +10,7 @@ Delayed(()=>{
 // type of a function is ()=> void
 // type of a int is number
 // type of a string is string
+
+
+//void is written coz fn is not returning anything
+//if there is any argument in fn , (a:number) => void
