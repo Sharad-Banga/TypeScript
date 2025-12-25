2f438588327882e865f6273c349af17e0da3a20b
Sharad-Banga <sharadiit03@gmail.com>
2025-12-25 13:57:05 +0530

    day 1 of revising ts

---
+++ Revv/src/07_moreTypes.ts
@@ -0,0 +1,17 @@
+type BooType = {
+  name : string;
+}
+
+let bookSring = `{"name":"sharad"}`;
+let bookObject = JSON.parse(bookSring);
+
+console.log(bookObject);
+
+//assertion
+
+//unknown
+
+
+//never
+
+
